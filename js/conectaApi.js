f9aba65e223c3a44d7ed48d2b418d869aabfb104
gustavojoze <gustavojosesoaressantana@gmail.com>
2025-08-24 19:39:51 -0300

    chore: trocando a URL da API para usar MockAPI

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,11 +1,11 @@
 async function listaProdutos() {
-    const conexao = await fetch("http://localhost:3000/produtos");
+    const conexao = await fetch("https://68ab8fb47a0bbe92cbb7c9cb.mockapi.io/produtos/produtos");
     const conexaoJson = await conexao.json();
     return conexaoJson;
 }
 
 async function criaProduto(nome, preco, url){
-    const conexao = await fetch("http://localhost:3000/produtos", {
+    const conexao = await fetch("https://68ab8fb47a0bbe92cbb7c9cb.mockapi.io/produtos/produtos", {
         method: "POST",
         headers:{
             "Content-type": "application/json"
@@ -22,7 +22,7 @@ async function criaProduto(nome, preco, url){
 }
 
 async function deletarProduto(id){
-    const idDelete = await fetch(`http://localhost:3000/produtos/${id}`,{
+    const idDelete = await fetch(`https://68ab8fb47a0bbe92cbb7c9cb.mockapi.io/produtos/produtos/${id}`,{
         method: "DELETE",
         headers:{
             "Content-type": "application/json"
